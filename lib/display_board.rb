f1b9140ce53f4773c54570fa150cbf4335392f55
Erica Albert <erica.albert@ibm.com>
2019-02-24 15:19:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,11 @@
 
 def 
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts 
+  puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
 
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
