f1f602c0849919a420c69abe39db95e7b6f30834
Erica Albert <erica.albert@ibm.com>
2019-02-24 15:19:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,9 @@
 
 def 
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts 
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
